b4874fde94b17ed52788090df78b3f3ec9fc3a1f
Jake <56444403+ChrisR443@users.noreply.github.com>
2022-10-15 15:51:25 -0400

    ee

--- http.py
+++ http.py
@@ -1,3 +1,7 @@
 import requests # pip install requests
 
 print(requests.get("https://google.com").text) # changed to Google
+
+import requests # pip install requests
+
+print(requests.get("https://google.com").text) # changed to Google
